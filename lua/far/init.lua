c1d56d53a2db8f227e3b9e1019462cc19e71a3b7
Moncii <50378234+Moncii@users.noreply.github.com>
2022-09-21 16:12:50 -0400

    Replace in file or selection.
    Replace in file or selection. All occurrences are nor replaced.

--- lua/far/init.lua
+++ lua/far/init.lua
@@ -2,7 +2,6 @@ local M = {}
 
 -- Keybindigs
 local map = vim.api.nvim_set_keymap
-local unmap = vim.api.nvim_del_keymap
 local opts = {noremap = true, silent = true}
 
 -- find_input and replace
@@ -12,83 +11,61 @@ local opts = {noremap = true, silent = true}
 local Input = require("nui.input")
 local event = require("nui.utils.autocmd").event
 
-M.findText = function(mode)
-
-    local replaceMode = mode
-    local find = vim.call("expand", "<cword>")
-
-    if(find ~= "") then
-
-        local replaceInput = Input({
-    
-            position = "50%",
-            size = {width = 40},
-
-            border = {
-                style = "rounded",
-                text = {
-                    top = '[ Replace "'..find..'" with? ]',
-                    top_align = "center",
-                }
-            },
-
-            win_options = {
-                winhighlight = "Normal:Normal,FloatBorder:Normal",
-            },
-    
-        }, {
-
-            prompt = "> ",
-        -- on_close = function()
-        --     print("Input Closed!")
-        -- end,
-            on_submit = function(value)
-                if(replaceMode == "file") then
-                    vim.cmd("%s/"..find.."/"..value)
-                elseif(replaceMode == "line") then
-                    vim.cmd("s/"..find.."/"..value)
-                end
-
-                vim.cmd('let @/ = ""')
-
-            end,
-
-        })
-
-        replaceInput:map("n", "<Esc>", function()
-            replaceInput:unmount()
-        end, {noremap = true})
-
-        replaceInput:mount()
-
-        replaceInput:on(event.BufLeave, function()
-            replaceInput:unmount()
-        end)
-
-    end
-
-    print("Place cursor over the word you would like to replace.")
-
-end
-
--- M.replaceText = function()
-
--- end
-
 M.findAndReplace = function(mode)
+    
+    local find = vim.call("expand", "<cword>")
+    local replace = nil
 
-    M.findText(mode)
-    -- vim.cmd("%s/"..find.."/"..replace)
+    local replaceInput = Input({
+        
+        position = "50%",
+        size = {width = 40},
+        border = {
+            style = "rounded",
+            text = {
+                top = ' Replace "'..find..'" with? ',
+                top_align  = "center",
+            }
+        },
+        win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:Normal",
+        },
+    }, {
+        prompt = "",
+        on_close = function()
+
+        end,
+        on_submit = function (value)
+           
+            replace = value
+
+            if(mode == "file") then
+                vim.cmd("%s/"..find.."/"..replace.."/g")
+            else
+                vim.cmd([[%s/\%V]]..find.."/"..replace.."/g")
+            end
+
+            vim.cmd('let @/ = ""')
+
+        end
+    }) 
+
+    replaceInput:mount()
+
+    replaceInput:on(event.BufLeave, function()
+        replaceInput:unmount()
+    end)
+
+    replaceInput:map("n", "<Esc>", function()
+        replaceInput:unmount()
+    end, { noremap = true })
 
 end
 
 M.setup = function(opts)
 
-    -- local bind = opts.map or "<leader>r"
-
     map("n", "<leader>fr", ":lua require'far'.findAndReplace('file')<CR>", opts)
-    map("n", "<leader>lr", ":lua require'far'.findAndReplace('line')<CR>", opts)
-    -- map("n", "<leader>fr", ":lua print'Hey!'", opts)
+    map("v", "<leader>sr", ":lua require'far'.findAndReplace('selection')<CR>", opts)
 
 end
 
